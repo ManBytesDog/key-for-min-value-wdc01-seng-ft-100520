dc6f3953a9d5205ae3bc565f8c4f5387f0f0290a
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-20 23:42:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
-  integer = nil
+  integer = nil 
   person = nil      
   name_hash.each do |name, value|
     if value < integer || value == nil 
