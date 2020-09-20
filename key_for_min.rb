9aac65785d03a56f076991e905fe72da3b724927
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-20 23:48:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   integer = nil 
   person = nil      
   name_hash.each do |name, value|
-    if value < integer
+    if integer < value || 
       integer = value
       person = name
     end
